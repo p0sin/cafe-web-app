dfeaca6da9d20d034b9451ce9e0605083be82449
p0sin <elposin@gmail.com>
2024-04-04 10:53:01 -0600

    Add extra elements to the form for more information about the cafe

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,16 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, SelectField, BooleanField, TimeField
 from wtforms.validators import DataRequired, URL
 
 class AddCafe(FlaskForm):
     name =  StringField('Name', validators=[DataRequired()])
     image_url = StringField('Image', validators=[DataRequired(), URL()])
     map_url = StringField('Google Maps Link', validators=[DataRequired(), URL()])
+    opening_hour = TimeField('Opening Hour')
+    closing_hour = TimeField('Closing Hour')
+    wifi = SelectField(u'Wifi Connection', choices=[('low', 'Low'), ('high', 'High')])
+    noise = SelectField(u'Noise Level', choices=[('low', 'Low'), ('high', 'High')])
+    pet_friendly = BooleanField('Pet Friendly')
+    electric_outlets = BooleanField('Electric Outlets')
     submit = SubmitField('Submit')
